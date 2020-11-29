986d70c2097419d55fee9989a804eab236fe0ca3
Saurabh Singh <Saurabh.s.kshatriya7@gmail.com>
2020-11-29 14:06:30 +0530

    calculating a value with functions

--- JS/functions.js
+++ JS/functions.js
@@ -1,6 +1,15 @@
+//performing a task
 function greet(name, lastName) {
     console.log("Hello " +name+ ' ' +lastName);
 }
 
 greet('Saurabh', 'Singh');
 greet('sexy');
+
+//calculating a value
+function square(number){
+    return number*number;
+}
+
+let number = square(45);
+console.log(number);
