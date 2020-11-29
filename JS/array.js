848477a82010bbb7c5ead1f957b87c14eef88c20
Saurabh Singh <Saurabh.s.kshatriya7@gmail.com>
2020-11-29 13:51:33 +0530

    arrays

---
+++ JS/array.js
@@ -0,0 +1,9 @@
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors);
+selectedColors[2] = 'green';
+console.log(selectedColors);
+console.log(selectedColors[0]);
+selectedColors[3] = 2; //you can store any type of values in array in js
+console.log(selectedColors);
+console.log(selectedColors.length); //length of an array
+
