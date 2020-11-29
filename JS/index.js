ac5ec18254ab4e33b877fea460d92093268435f7
Saurabh Singh <Saurabh.s.kshatriya7@gmail.com>
2020-11-29 13:06:06 +0530

    js with node

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,2 @@
-var x = 4;
-var y = 5;
-var z = x+y;
-print(z);
-
-var a = SimpleImage('nothing.png');
-var b = SimpleImage('something.png');
-
-var c = b.getWidth();
-//this is somehow it works
+//this is my first javascript code
+console.log('Hello World');
